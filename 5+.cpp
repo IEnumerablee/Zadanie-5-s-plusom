5bebfd4ad27d7d1df92f1a923df1cebc36754a28
IEnumerablee <thepashalza@gmail.com>
2025-03-19 15:46:09 +0500

    refactor: fix typo

--- 5+.cpp
+++ 5+.cpp
@@ -3,7 +3,7 @@
 
 #define CHUCHUT 1
 
-void vstotonu(std::string &str, int v) {
+void vstoronu(std::string &str, int v) {
 	for (char &c : str) c <<= v;
 }
 
@@ -12,9 +12,9 @@ int main() {
     std::string str2 = "Отлично";
     std::string str3 = "Отлично";
 	
-	vstotonu(str1, CHUCHUT);
-	vstotonu(str2, CHUCHUT);
-	vstotonu(str3, CHUCHUT);
+	vstoronu(str1, CHUCHUT);
+	vstoronu(str2, CHUCHUT);
+	vstoronu(str3, CHUCHUT);
 
 	std::printf("\n%s\n%s\n%s\n", str1.c_str(), str2.c_str(), str3.c_str());
     
